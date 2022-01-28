bcdac9d3f36a0d767d580e670e3b88deafeb68c5
james-muriithi <muriithijames556@gmail.com>
2022-01-29 01:26:10 +0300

    added configurations

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+from decouple import config
+
+class Config:
+    """
+    General configuration parent class
+    """
+    NEWS_API_KEY = config('NEWS_API_KEY')
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'.format(NEWS_API_KEY)
+    NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    
+    SECRET_KEY = config('SECRET_KEY')
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+config_map = {
+    'development': DevelopmentConfig,
+    'production': ProdConfig
+}
