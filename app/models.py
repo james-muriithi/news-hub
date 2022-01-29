0245531e02e5454dd0657ddc9a0bbcf7a736adbe
james-muriithi <muriithijames556@gmail.com>
2022-01-29 14:42:44 +0300

    added articles model

--- app/models.py
+++ app/models.py
@@ -12,3 +12,16 @@ class Sources:
         self.country = country
 
 
+class Articles:
+    """
+    Articles class to define articles object
+    """
+
+    def __init__(self, author, title, description, url, urlToImage, publishedAt, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
