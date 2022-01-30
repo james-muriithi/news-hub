4b14d609bcd09cda921a010015a4d4589ca3cde7
james-muriithi <muriithijames556@gmail.com>
2022-01-30 13:09:59 +0300

    added api key fallback

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import os
 from decouple import config
 
@@ -5,7 +6,7 @@ class Config:
     """
     General configuration parent class
     """
-    NEWS_API_KEY = config('API_KEY')
+    NEWS_API_KEY = config('API_KEY', default="")
     NEWS_API_BASE_URL = 'https://newsapi.org/v2'
     NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'.format(NEWS_API_KEY)
     NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
